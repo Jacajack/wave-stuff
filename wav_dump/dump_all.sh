5309d849143e0d450c3b0e58811d8fced6b871c3
Jacek Wieczorek <mrjjot@gmail.com>
2025-11-14 20:35:44 +0100

    Dump all waveforms too

--- wav_dump/dump_all.sh
+++ wav_dump/dump_all.sh
@@ -6,6 +6,12 @@ SAMPLERATE=8000
 mkdir -p wav
 
 for n in {0..28}; do
-	./ppg_wt_dump $n 10 | ./mkwav "wav/long_${n}.wav" $SAMPLERATE
-	./ppg_wt_dump $n 1 | ./mkwav "wav/${n}.wav" $SAMPLERATE
+	./ppg_wt_dump $n 10 | ./mkwav "wavetables/long_${n}.wav" $SAMPLERATE
+	./ppg_wt_dump $n 1 | ./mkwav "wavetables/${n}.wav" $SAMPLERATE
 done;
+
+for n in {0..255}; do
+	./ppg_wave_dump $n 100 | ./mkwav "waveforms/wave_long_${n}.wav" $SAMPLERATE
+	./ppg_wave_dump $n 1 | ./mkwav "waveforms/wave_${n}.wav" $SAMPLERATE
+done;
+
